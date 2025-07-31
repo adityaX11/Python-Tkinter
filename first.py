c355109d98172865f66e0c2d45c5484a3568ba60
Aditya kumar <adicpr1234@gmail.com>
2025-08-01 01:07:42 +0530

    learn tkinter..

--- first.py
+++ first.py
@@ -14,4 +14,13 @@ root.iconbitmap(r"C:\Users\Aditya Kumar\Downloads\icon.ico")
 # root.config(bg="red")
 root['bg'] = "#3b253b"
 
+root.geometry("500x400-150-150") # by this you can resize the gui interface and set point where you want to view the Interface
+
+# for set the position on center of system of GUI interface.
+width=300
+height=400
+
+sys_width = root.win
+
+
 root.mainloop() ## by the help of this command you run the tkinter page
